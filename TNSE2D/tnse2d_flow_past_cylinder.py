c6557f339fde7cf3bb9ecf5b6041ed504662f506
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-08-24 13:12:32 +0100

    Working commit for flow past cylinder

--- TNSE2D/tnse2d_flow_past_cylinder.py
+++ TNSE2D/tnse2d_flow_past_cylinder.py
@@ -1,13 +1,18 @@
 from firedrake import *
 from firedrake.petsc import PETSc
+import os
 
 N = 64
 
 mesh = Mesh("assets/meshes/flow_past_cylinder.msh")
+output_dir = "tnse2d_output"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
 
 dt = 1e-3
 T = 5e-1
 t = 0.0
+c = Constant(t)
 
 def du_dt(u, u_n, dt):
     return (u - u_n) / dt
@@ -38,19 +43,12 @@ boundary_markers = {"Bottom Wall": 1,
                     "Top Wall": 3,
                     "Inlet": 4,
                     "Cylinder": 5}
+
 no_slip_bc = Constant((0.0, 0.0))
 bcs = [DirichletBC(Z.sub(0), no_slip_bc, (boundary_markers["Bottom Wall"], boundary_markers["Top Wall"], boundary_markers["Cylinder"]))]
 
-
-
-# bcs = [DirichletBC(Z.sub(0),Constant((1.0,0.0)),(4,)), DirichletBC(Z.sub(0),Constant((0.0,0.0)),(1,2,3))]
-# bcs = [DirichletBC(Z.sub(0),Constant((1.0,0.0)),(4,)), DirichletBC(Z.sub(0),Constant((0.0,0.0)),(1,2,3))]
-c = Constant(t)
-
 bcs.append(DirichletBC(Z.sub(0),(inlet_velocity(y, c),0.0), (boundary_markers["Inlet"],)))
 bcs.append(DirichletBC(Z.sub(1), Constant(0.0), (boundary_markers["Outlet"],)))
-
-
 
 Re = Constant(100.0)
 F = (
@@ -61,21 +59,31 @@ F = (
     div(u)*phi*dx
 )
 
-nullspace = MixedVectorSpaceBasis(
-    Z, [Z.sub(0), VectorSpaceBasis(constant=True)])
+solver_parameters = {
+    "snes_type": "newtonls",
+    "snes_monitor": None,
+    "snes_rtol": 1e-6,
+    "ksp_type": "gmres",
+    "pc_type": "fieldsplit",
+    "pc_fieldsplit_type": "schur",
+    "pc_fieldsplit_schur_fact_type": "full",
+    "fieldsplit_0_ksp_type": "preonly",
+    "fieldsplit_0_pc_type": "hypre",
+    "fieldsplit_1_ksp_type": "preonly",
+    "fieldsplit_1_pc_type": "hypre"
+}
+problem = NonlinearVariationalProblem(F, up, bcs=bcs)
+solver = NonlinearVariationalSolver(problem, solver_parameters=solver_parameters)
 
-
-outfile = VTKFile("lid_driven_cavity.pvd")
+outfile = VTKFile(f"{output_dir}/flow_past_cylinder.pvd")
 while t < T:
+    print(f"t = {t}")
     up.assign(up_n)
-    solve(F == 0, up, bcs=bcs, nullspace=nullspace,
-                    solver_parameters={"snes_monitor": None,
-                             "ksp_type": "gmres",
-                             "mat_type": "aij",
-                             "pc_type": "lu",
-                             "pc_factor_mat_solver_type": "mumps"})
+    solver.solve()
+
     up_n.assign(up)
     t += dt
+    c.assign(t)
     u, p = up.subfunctions
     u.rename("velocity")
     p.rename("pressure")

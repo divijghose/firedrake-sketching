7c0b772f31b08011d7f3f35b65616b6e910b2755
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-08-25 15:42:15 +0100

    Refactored Cahn-Hillard code that takes inputs from a yaml file

--- TCH/cahn-hillard.py
+++ TCH/cahn-hillard.py
@@ -3,19 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from firedrake.ufl_expr import ufl
+import sys
 
-output_dir = "ch_output"
+# Read configuration from YAML file
+if len(sys.argv) > 1 and sys.argv[1].endswith(".yaml"):
+    PETSc.Sys.Print(f"Reading configuration from YAML file: {sys.argv[1]}")
+    yaml_file_path = sys.argv[1]
+    try:
+        import yaml
+    except ImportError:
+        raise ImportError("PyYAML is required to read YAML files.")
+    with open(yaml_file_path, "r") as file:
+        config = yaml.safe_load(file)
+else:
+    raise ValueError("Please provide a YAML configuration file as a command-line argument.")
+
+
+output_dir = config.get("output_dir", "ch_output")
+os.makedirs("results", exist_ok=True)
 if not os.path.exists(output_dir):
+    output_dir = f"results/{output_dir}"
     os.makedirs(output_dir, exist_ok=True)
+outfile = VTKFile(f"{output_dir}/tch.pvd")
 
-N = 100
-L = 1.0
+N = config.get("num_elems", 100)
 mesh = UnitSquareMesh(N, N)
 
-lmbda = 1e-2
-dt = 5e-6
-theta = 0.5
+lmbda = float(config.get("lambda", 1e-2))
+dt = float(config.get("dt", 5e-6))
+theta = float(config.get("theta", 0.5))
 t = 0.0
+T = 50*dt
+tdump = config.get("output_frequency", 10) * dt
+dumpt = 0.
 
 
 V = FunctionSpace(mesh, "CG", 1)
@@ -33,15 +53,13 @@ pcg = PCG64(seed=42)
 rg = RandomGenerator(pcg)
 u_init = rg.random(V)
 u_init.assign(0.5 + 0.01 * (0.5 - u_init))
-
 u.sub(0).assign(u_init)
-
 
 c = ufl.variable(c)
 f = 100 * c**2 * (1 - c)**2
 dfdc = ufl.diff(f, c)
 
-mu_mid = (1 - theta) * mu0 + theta * mu 
+mu_mid = (1 - theta) * mu0 + theta * mu # Crank-Nicholson for theta = 0.5
 
 F0 = (
     inner(c, q) * dx
@@ -68,18 +86,22 @@ solver = NonlinearVariationalSolver(problem, solver_parameters={
     "pc_factor_mat_solver_type": "mumps",
     "snes_monitor": None,
 })
-file = VTKFile(f"{output_dir}/tch.pvd")
+
+
 c_plot, mu_plot = u.subfunctions
-file.write(c_plot)
+outfile.write(c_plot)
 u0.assign(u)
-T = 50*dt
+
 while t < T:
     t += dt
-
-    print(f"t = {t}")
+    if config.get("verbose", True):
+        PETSc.Sys.Print(f"t = {t}")
     solver.solve()
     u0.assign(u)
 
-    c_plot, mu_plot = u.subfunctions
 
-    file.write(c_plot)
+    if dumpt > tdump - dt/2:
+        c_plot, mu_plot = u.subfunctions
+        outfile.write(c_plot)
+        dumpt -= tdump
+    dumpt += dt
